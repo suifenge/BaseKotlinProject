4a8ee060264bb1b315d112af4c617f328b42fdb5
liujiachun <liujiachun@funtsui.com>
2018-06-19 17:25:20 +0800

    增加文件

---
+++ kotlinbase/src/main/java/com/suifeng/kotlin/base/ui/BaseActivity.kt
@@ -0,0 +1,14 @@
+package com.suifeng.kotlin.base.ui
+
+import com.trello.rxlifecycle2.components.support.RxAppCompatActivity
+
+/**
+ * @author ljc
+ * @data 2018/6/19
+ * @describe
+ */
+abstract class BaseActivity(
+
+) : RxAppCompatActivity() {
+    
+}
